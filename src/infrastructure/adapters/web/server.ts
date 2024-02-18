9cfbfce3f62f7a4411c4636ef8377ba1ddfd4874
nicopatoco <nicolasherrera.cp@gmail.com>
2024-02-18 21:12:25 +0100

    feat: add server improvements

--- src/infrastructure/adapters/web/server.ts
+++ src/infrastructure/adapters/web/server.ts
@@ -1,8 +1,14 @@
 import express from 'express';
 import userRoutes from './routes/userRoutes';
+import corse from 'cors';
+import morgan from 'morgan';
 
 const app = express();
+
+app.use(corse()); // to allow cross origin request
+app.use(morgan('dev')); // dev logging
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Use the user routes
 app.use('/api/users', userRoutes);
